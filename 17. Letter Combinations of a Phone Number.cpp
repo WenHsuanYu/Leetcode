04b1aa02e9640e7bbc29287597392e017e51e2ae
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-02-11 14:40:22 +0800

    Add a 17. Letter Combinations of a Phone Number

---
+++ 17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> res;
+    
+    void dfs(int idx, string& digits, unordered_map<int, string>& m, string& ans) {
+        if(idx == digits.length()){
+            res.push_back(ans);
+            return;
+        }
+        int curr = digits[idx] - '0';
+        string op = m[curr];
+        
+        for (int i = 0; i < op.length(); i++) {
+            ans.push_back(op[i]);
+            dfs(idx + 1, digits, m, ans);
+            ans.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if (digits.size() == 0)
+            return {};
+        unordered_map<int, string> map = {
+            {2, "abc"},
+            {3, "def"},
+            {4, "ghi"},
+            {5, "jkl"},
+            {6, "mno"},
+            {7, "pqrs"},
+            {8, "tuv"},
+            {9, "wxyz"}
+        };
+        string ans = "";
+        dfs(0, digits, map, ans);
+        return res;
+    }
+};
