ecd2ad31e009da0b30dd8daf320f59a2766481a8
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-05-26 13:04:43 +0800

    Add 55. Jump Game_DP_Bottom_up.cpp

---
+++ CandidateSolution/55. Jump Game_DP_Bottom_up.cpp
@@ -0,0 +1,21 @@
+//DP (Bottom-up)
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        //traverse backward
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && j + nums[j] >= i) {
+                    dp[i] = true;
+                    break;
+                }
+                
+            }
+        }
+        
+        return dp[n -1];
+    }
+};
