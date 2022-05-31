a01ee2e2ec4e8c56b4be5d234a9bb148a108b421
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-05-31 12:28:26 +0800

    Add a 46. Permutations_DFS_variant.cc

---
+++ CandidateSolution/46. Permutations_DFS_variant.cc
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> temp, visited;
+    vector<vector<int>> res;
+    
+    void dfs(vector<int>& nums, int level) {
+        
+        if (level == nums.size()) {
+            res.push_back(temp);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (visited[i] == true)
+                continue;   
+            
+            visited[i] = 1;
+            temp.push_back(nums[i]);
+            dfs(nums, level + 1);
+            temp.pop_back();
+            visited[i] = 0; 
+        }
+        
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        visited.resize(nums.size(), 0);
+        dfs(nums, 0);
+        
+        
+        return res;
+    }
+};
