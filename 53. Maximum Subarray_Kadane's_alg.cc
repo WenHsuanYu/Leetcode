f8cab030780fd6533554b82eb64a90d916979b7f
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-05-31 16:03:04 +0800

    Add a 53. Maximum Subarray_Kadane's_alg.cc

---
+++ 53. Maximum Subarray_Kadane's_alg.cc
@@ -0,0 +1,23 @@
+static auto out = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+
+//Kadane's algorithm
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr_max = 0;
+        int ans = INT_MIN;
+        for (auto i : nums) {
+            curr_max += i;
+            ans = max(ans, curr_max);
+            curr_max = curr_max < 0 ? 0 : curr_max;
+        }
+        
+        return ans;
+    }
+};
