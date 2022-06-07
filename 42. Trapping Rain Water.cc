36250e31e03757db898fb9779e1feb3662535f7a
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-06-08 00:07:18 +0800

    Add a 42. Trapping Rain Water.cc

---
+++ 42. Trapping Rain Water.cc
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        int maxL = height[i];
+        int maxR = height[j];
+        int res = 0;
+        int h;
+        
+        while (i < j) {
+            if (height[i] > height[j]) {
+                h = height[j];
+                if (h > maxR) {
+                    maxR = h;
+                } else {
+                    res += maxR - h;
+                }
+                j--;
+            } else {
+                h = height[i];
+                if (h > maxL) {
+                    maxL = h;
+                } else {
+                    res += maxL - h;
+                    
+                }
+                i++;
+            }
+        
+        }
+        
+        return res;
+    }
+};
