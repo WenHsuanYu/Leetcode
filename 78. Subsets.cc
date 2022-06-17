043085bd5658481b95836ac050cd4dec3c999e33
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-06-18 00:20:22 +0800

    Add a 78. Subsets.cc

---
+++ 78. Subsets.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void backtrack(vector<int> &nums, int i, vector<int> temp)
+    {
+        if(i == nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        backtrack(nums, i+1, temp);
+        temp.push_back(nums[i]);
+        backtrack(nums, i+1, temp);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;       
+        backtrack(nums, 0, temp);
+        return ans;
+    }
+};
