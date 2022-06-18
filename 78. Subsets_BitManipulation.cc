c9dabbfb74435c99d178e1f633fc009f8b03d067
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-06-19 00:10:27 +0800

    Add a 78. Subsets_BitManipulation.cc

---
+++ 78. Subsets_BitManipulation.cc
@@ -0,0 +1,55 @@
+//using bit manipulation method
+//TC: O(2^N * N)
+//SC: O(N)
+#define bitmask_len 11
+class Solution {
+public:
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        //The length of nums
+        int n = nums.size();
+        int sub = pow(2, n);
+        vector<vector<int>> ans(sub, vector<int>());
+        int row = 0;
+        int len; 
+        int i = 0;
+        while (i < sub){
+            string bitmask = bitset<bitmask_len>(i).to_string();
+        
+            for (int j = bitmask_len - n; j < bitmask_len; ++j) {
+                if (bitmask[j] == '1') {
+                    ans[row].push_back(nums[j - bitmask_len + n]);
+                }
+            }
+            ++row;
+            ++i;
+        }
+        return ans;
+    }
+};
+
+/* Alternative method:
+ * Using bit manipulation method
+ * TC: O(2^N * N)
+ * SC: O(N)
+ * class Solution {
+ * public:
+ *     vector<vector<int>> subsets(vector<int>& nums) {
+ *         vector<vector<int>> ans;
+ *         int n = nums.size();
+ *         int sub = pow(2, n);
+ *         int i = 0;
+ *        while (i < sub) {
+ *            vector<int> temp;
+ *            for (int j = 0; j < n; ++j) {
+ *                if (i & (1 << j)) {
+ *                    temp.push_back(nums[j]);
+ *                }
+ *             }
+ *             ans.push_back(temp);
+ *              ++i;
+ *         }
+ *        return ans;
+ *    }
+ * };
+**/
