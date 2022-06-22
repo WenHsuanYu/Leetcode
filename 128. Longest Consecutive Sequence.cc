6e34b3403d43eaed8abba6feb85e3027b45d07a9
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-06-22 17:05:37 +0800

    Add a 128. Longest Consecutive Sequence.cc

---
+++ 128. Longest Consecutive Sequence.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        unordered_set<int> uset(begin(nums), end(nums));
+        int n = nums.size();
+        int longestSequence = 0;
+        for (auto &val : uset) {
+            //C++20 can use uset.contains(key)
+            if (uset.count(val - 1))
+                continue;
+            int j = 1;
+            while (uset.count(val + j))
+                j++;
+            longestSequence = max(j, longestSequence);
+        }        
+        
+        return longestSequence;
+    }
+};
