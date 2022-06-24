2083be85478000c501bcae6aae421260ed95c2b7
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-06-24 23:49:04 +0800

    Add a 215. Kth Largest Element in an Array_StdSort.cc

---
+++ 215. Kth Largest Element in an Array_StdSort.cc
@@ -0,0 +1,10 @@
+//Using std::sort method
+//TC:O(nlogn)
+//SC:O(1)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() - k];
+    }
+};
