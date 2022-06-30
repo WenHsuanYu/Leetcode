6f93c5842aa6f124a2ff74c1cf5d28304e2df74a
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-06-30 16:17:37 +0800

    Add a Longest Increasing Subsequence_DP.cc

---
+++ Longest Increasing Subsequence_DP.cc
@@ -0,0 +1,22 @@
+//Using DP method
+//TC: O(n^2)
+//SC: O(n)
+class Solution {
+public:
+        
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        
+        int lis = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            lis = dp[i] > lis ? dp[i] : lis;
+        }
+        return lis;
+    }
+};
