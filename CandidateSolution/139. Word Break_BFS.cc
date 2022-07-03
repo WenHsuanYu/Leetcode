2c161e7a66ab591b65a3c165768812e07e02c4d8
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-04 01:01:41 +0800

    Add 139. Word Break_BFS.cc

---
+++ CandidateSolution/139. Word Break_BFS.cc
@@ -0,0 +1,31 @@
+// Using BFS method
+// TC: O(n^3)
+// SC: O(n)
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> word_set(wordDict.begin(), wordDict.end());
+        vector<bool> visited(s.size(), false);
+        queue<int>q;
+        q.push(0);
+        while(!q.empty()) {
+            int start = q.front();
+            q.pop();
+            if (visited[start]) {
+                continue;
+            }
+            
+            for (int end = start + 1; end <= s.size(); end++) {
+                if (word_set.find(s.substr(start, end - start)) != word_set.end()) {
+                    q.push(end);
+                    if (end == s.length())
+                        return true;
+                }   
+                
+            }
+            visited[start] = true;
+        }
+        return false;
+            
+    }
+};
