8b56b9292736eb1fd0be1fcf047f7ccb58435328
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-06 00:44:12 +0800

    Add a 160. Intersection of Two Linked Lists

---
+++ 160. Intersection of Two Linked Lists_spaceConstant.cc
@@ -0,0 +1,25 @@
+//TC:O(m + n)
+//SC:O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* ptrA = headA;
+        ListNode* ptrB = headB;
+        // ptrA: [A + common + B] + common
+        // ptrB: [B + common + A] + common
+        while (ptrA != ptrB) {
+            ptrA = ptrA ? ptrA->next : headB;
+            ptrB = ptrB ? ptrB->next : headA;
+        }
+        
+        return ptrA;
+    }
+};
