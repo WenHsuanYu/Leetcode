01f7a62a2daefb046a08a468812c031947c9ff3f
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-06 14:09:16 +0800

    Add a 96. Unique Binary Search Trees.cc

---
+++ 96. Unique Binary Search Trees.cc
@@ -0,0 +1,49 @@
+//Using DP(bottom up) method
+//TC: O(n^2)
+//SC: O(n)
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int>dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        
+        return dp[n];
+    }
+};
+
+/*
+* class Solution {
+* public:
+*    vector<int>dp;
+*     
+*     int recur(int n) {
+*         if (n == 0 || n == 1) {
+*             return 1;
+*         }
+*         if (n == 2)
+*             return 2;
+*         if (dp[n] != 0) {
+*             return dp[n];
+*         }
+*         int sum = 0;
+*         for (int i = 1; i <= n; i++)
+*             sum += (recur(i - 1) * recur(n - i));
+*         dp[n] = sum;
+*         return dp[n];
+*     }
+*     
+*     
+*     int numTrees(int n) {
+*         dp.resize(n + 1, 0);
+*         dp[0] = 1;
+*         dp[n] = recur(n);
+*         return dp[n];
+*     }
+* };
+*/
