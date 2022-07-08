7383cd70beb4f218e05c7dfc07274812a91df51b
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-08 15:14:05 +0800

    Add a 94. Binary Tree Inorder Traversal_MorrisTraversal.cc

---
+++ 94. Binary Tree Inorder Traversal_MorrisTraversal.cc
@@ -0,0 +1,46 @@
+// Using Morris Traversal (Threaded Binary tree) method
+//TC: O(n)
+//SC: O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (!root) {
+            return ans;
+        }
+        
+        TreeNode* curr = root;
+        TreeNode* pre;
+        while (curr) {
+            if (!curr->left) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                pre = curr->left;
+                while (pre->right) {
+                    pre = pre->right;
+                }
+                pre->right = curr;
+                TreeNode* temp = curr;
+                curr = curr->left;
+                temp->left = nullptr;
+                
+            }
+        
+        }
+        return ans;
+        
+    }
+};
