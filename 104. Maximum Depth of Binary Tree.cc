7ded718b0568cd362c7c159a4208a5ff851907ed
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-08 20:18:21 +0800

    Add a 104. Maximum Depth of Binary Tree.cc

---
+++ 104. Maximum Depth of Binary Tree.cc
@@ -0,0 +1,26 @@
+//Using recursion method
+//TC: O(n)
+//SC: O(logn)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+        
+    int maxDepth(TreeNode* root) {
+        if(!root) {
+            return 0;
+        }
+        int depthL = maxDepth(root->left);
+        int depthR = maxDepth(root->right);
+        return 1 + max(depthL, depthR);
+    }
+};
