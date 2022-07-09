b6192dc76b1e33fe336d0075c802d261800af454
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-09 15:24:22 +0800

    Add a 207. Course Schedule.cc

---
+++ 207. Course Schedule.cc
@@ -0,0 +1,39 @@
+// Using topological sort (kahn's algorithm)
+// TC: O(V+E)
+// SC: O(V)
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses, vector<int>());
+        vector<int> indegree(numCourses, 0);
+        //O(E)
+        for (auto &p: prerequisites) {
+            adjList[p[1]].push_back(p[0]);
+            indegree[p[0]]++;
+        }
+        //O(V)
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+
+            }
+        }
+        
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            numCourses--;
+            for (auto vertex: adjList[curr]) {
+                if(--indegree[vertex] == 0) {
+                    q.push(vertex);
+                }
+            }
+        }
+        
+        
+        return numCourses == 0;
+        
+        
+    }
+};
