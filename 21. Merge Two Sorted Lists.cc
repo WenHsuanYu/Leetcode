7309fb78605301e473bc360aaa6f868131f18be2
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-13 12:18:40 +0800

    Fix code

--- 21. Merge Two Sorted Lists.cc
+++ 21. Merge Two Sorted Lists.cc
@@ -5,51 +5,23 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       
-	    // if list1 happen to be NULL
-		// we will simply return list2.
-        if(list1 == NULL)
-            return list2;
-		
-		// if list2 happen to be NULL
-		// we will simply return list1.
-        if(list2 == NULL)
+        if (list1 == nullptr) {
+            return list2;   
+        }
+        if (list2 == nullptr) {
             return list1;
+        }
         
-        ListNode * ptr = list1;
-        if(list1 -> val > list2 -> val)
-        {
-            ptr = list2;
-            list2 = list2 -> next;
+        if (list1->val <= list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
-        else
-        {
-            list1 = list1 -> next;
-        }
-        ListNode *curr = ptr;
         
-		// till one of the list doesn't reaches NULL
-        while(list1 &&  list2)
-        {
-            if(list1 -> val < list2 -> val){
-                curr->next = list1;
-                list1 = list1 -> next;
-            }
-            else{
-                curr->next = list2;
-                list2 = list2 -> next;
-            }
-            curr = curr -> next;
-                
-        }
-		
-		// adding remaining elements of bigger list.
-        if(!list1)
-            curr -> next = list2;
-        else
-            curr -> next = list1;
-            
-        return ptr;
-       
     }
+
 };
+
+
