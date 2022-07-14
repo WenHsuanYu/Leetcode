94871bdfccbd1fbe3de594a5a20fd0eabb6b79ec
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-14 23:16:54 +0800

    Add a 30. Kth Smallest Element in a BST

---
+++ 230. Kth Smallest Element in a BST.cc
@@ -0,0 +1,38 @@
+// TC: O(H + K), where H is a tree height
+// SC: O(H)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+
+        vector<int> res;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while (curr || !st.empty()) {
+            if(curr) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                curr = st.top();
+                k--;
+                if (k == 0) {
+                    return curr->val;
+                }
+                res.push_back(curr->val);
+                st.pop();
+                curr = curr->right;
+            }
+        }
+        return res[k - 1];
+    }
+};
