6d20e09ce516b6fa144598a2f9d5b45f78494436
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-07-15 22:48:43 +0800

    Add a 13. Roman to Integer.cc

---
+++ 13. Roman to Integer.cc
@@ -0,0 +1,29 @@
+// TC: O(n)
+// SC: O(n)
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, uint32_t> map;
+        map['I'] = 1;
+        map['V'] = 5;
+        map['X'] = 10;
+        map['L'] = 50;
+        map['C'] = 100;
+        map['D'] = 500;
+        map['M'] = 1000;
+        
+        int ans = 0;
+        int len = s.length();
+        ans += map[s[0]];
+        for (int i = 1; i < len; i++) {
+            if (map[s[i - 1]] < map[s[i]]) {
+                ans += -2 * map[s[i - 1]] + map[s[i]];
+            } else {
+                ans += map[s[i]];
+            }
+        }
+        
+        
+        return ans;
+    }
+};
