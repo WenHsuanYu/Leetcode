4e84145b337e4cc9119146a89c208c68a2cd568c
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-08-12 10:51:05 +0800

    Add a 8. String to Integer (atoi).cc

---
+++ 8. String to Integer (atoi).cc
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int num = 0;
+        int index = 0;
+        int sign = 1;
+        while (s[index] == ' ') {
+            index++;
+        }
+        
+        if (s[index] == '-' || s[index] == '+') {
+            sign = 1 - 2 * (s[index] == '-');
+            index++;
+        }
+        
+        
+        while (s[index] >= '0' && s[index] <= '9') {
+            if (num > INT_MAX / 10 || (num == INT_MAX / 10 && s[index] - '0' > INT_MAX % 10)) {
+                if (sign == 1) {
+                    return INT_MAX;
+                } else {
+                    return INT_MIN;
+                }
+            }
+            num = num * 10 + (s[index] - '0');
+            index++;
+        }
+        return num * sign;
+    }
+};
