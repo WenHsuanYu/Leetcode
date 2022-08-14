9f05a4c2ae87640f700e1c6aa4d37f0fe93d888a
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-08-14 13:06:13 +0800

    Add a 83. Remove Duplicates from Sorted List_recursion.cc

---
+++ 83. Remove Duplicates from Sorted List_recursion.cc
@@ -0,0 +1,23 @@
+// TC: O(n)
+// SC: O(n)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !(head->next)){
+            return head;
+        }
+        head->next = deleteDuplicates(head->next);
+        return head->val == head->next->val ? head->next : head;
+        
+    }
+};
