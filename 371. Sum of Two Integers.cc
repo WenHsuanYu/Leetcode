bc77e603cec0441733ba93464a4733a9396b3e42
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-11-03 16:55:04 +0800

    Add 371. Sum of Two Integers.cc

---
+++ 371. Sum of Two Integers.cc
@@ -0,0 +1,13 @@
+//TC: O(1)
+//SC: O(1)
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b) {
+            int carry = (a & b & 0x7fffffff) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+};
