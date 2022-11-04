49f2feabf9369270b2f9ed5174b75f63d10494b3
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-11-04 20:59:57 +0800

    Add a 2455. Average Value of Even Numbers That Are Divisible by Three

---
+++ 2455. Average Value of Even Numbers That Are Divisible by Three.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int averageValue(vector<int>& nums) {
+        int count = 0;
+        int len = nums.size();
+        int sum = 0;
+        for (const auto &i: nums) {
+            cout << i << endl;
+            if ((i & 1) == 0 && i % 3 == 0) {
+                cout << i << endl;
+                sum += i;
+                count++;
+            }
+            
+        }
+        return count != 0 ? sum / count : count ;
+    }
+};
