7d577f1b773773a17f4c96d9ef905bb41bfba93d
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-11-05 22:49:56 +0800

    Remove a redundant charcter

--- 21. Merge Two Sorted Lists_iterative.cc
+++ 21. Merge Two Sorted Lists_iterative.cc
@@ -30,7 +30,7 @@ public:
         }
         ListNode* Merged_tail = Merged_head;
         
-        while (list1 && list2) {A
+        while (list1 && list2) {
             if (list1->val < list2->val) {
                 Merged_tail->next = list1;
                 list1 = list1->next;
