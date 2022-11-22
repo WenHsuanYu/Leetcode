a4f9f5724cc72f6bc6f8b9ee2adc293bdc6a3735
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-11-22 16:21:21 +0800

    More efficient code

--- 240. Search a 2D Matrix II_SpaceReduced.cc
+++ 240. Search a 2D Matrix II_SpaceReduced.cc
@@ -4,19 +4,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        ios_base::sync_with_stdio(false);
         int m = matrix.size();
         int n = matrix[0].size();
-        int row = 0;
-        int col = n - 1;
-        while(row < m && col >= 0) {
-            if (matrix[row][col] == target)
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] > target) {
+                j--;
+            } else if (matrix[i][j] == target) {
                 return true;
-            else if (target > matrix[row][col])
-                row++;
-            else
-                col--;
-                
+            } else {
+                i++;
+            }
         }
         return false;
     }
