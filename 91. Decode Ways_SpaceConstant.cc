9e2cba6e50ad9fc383d417720c88eabaae34a225
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-11-27 12:08:54 +0800

    Reduce code

--- 91. Decode Ways_SpaceConstant.cc
+++ 91. Decode Ways_SpaceConstant.cc
@@ -3,24 +3,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        if (s[0] == '0') 
-            return 0;
-        //when position is at index i, first_front means the number of way of decoding at i - 1 
-        int first_front = 1;
-        //when position is at index i, first_front means the number of way of decoding at i - 2 
-        int second_front = 1;
         int len = s.size();
-        for (int i = 1; i < len; ++i) {
-            if (s[i] == '0') 
-                first_front = 0;
-            if (s[i - 1] == '1' || (s[i - 1] == '2' && s[i] <= '6')) {
-                //renew
-                first_front = first_front + second_front;
-                second_front = first_front - second_front;
-            } else {
-                second_front = first_front;
+        int last1 = 1;
+        int last2 = 1;
+        //We can use last1 and last2 to perform constant space dp since dp depends only on
+        //last two values  
+        for (int i = 0; i < len; i++) {
+            int tmp = 0;
+            if (s[i] > '0') {
+                tmp += last1;
             }
+            if (i && (s[i - 1] == '1' || s[i - 1] == '2' && s[i] >= '0' && s[i] <= '6')) {
+                tmp += last2;
+            }
+            last2 = last1;
+            last1 = tmp;
         }
-        return first_front;
+        return last1;
     }
 };
