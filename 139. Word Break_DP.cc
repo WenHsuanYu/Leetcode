f822b60dd5d7ed68ed49ada1bf9d4d8cba5fb750
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-11-30 15:45:09 +0800

    Reduce Code

--- 139. Word Break_DP.cc
+++ 139. Word Break_DP.cc
@@ -6,16 +6,20 @@ class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         //dp[i] means 0...i can be separated by words in wordDict
-        vector<bool> dp(s.size() + 1, false);
+        int len = s.size();
+        vector<int> dp(len + 1, 0);
+        dp[0] = 1;
         /*
         *    l e e t c o d e
         *  0 1 2 3 4 5 6 7 8
         */
-        dp[0] = true;
-        for (int i = 1; i <= s.size(); i++) {
-            for (int j = 0; j < wordDict.size(); j++) {
-                if (i >= wordDict[j].size() && wordDict[j] == s.substr(i - wordDict[j].size(), wordDict[j].size()) && dp[i - wordDict[j].size()]) {
-                    dp[i] = true;
+        
+        int lenDict = wordDict.size();
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < lenDict; j++) {
+                int lenWord = wordDict[j].size();
+                if (i >= lenWord && wordDict[j] == s.substr(i - lenWord, lenWord) && dp[i - lenWord]) {
+                    dp[i] = 1;
                     break;
                 }
             }
