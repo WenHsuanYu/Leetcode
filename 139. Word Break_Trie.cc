14afdd900be91fae59396699d173b88a4efaaa1a
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-11-30 15:48:20 +0800

    Fix potential memory leak

--- 139. Word Break_Trie.cc
+++ 139. Word Break_Trie.cc
@@ -1,53 +1,19 @@
 //Using Trie + DP method
 //TC: O(max(N^2, S)), where N is the length of the string and S is the sum of length of all words in the dictionary
 //SC: O(26*S + N)
-class TrieNode {
-public:
-    vector<TrieNode*> alpha;
-    bool isEnd;
-    TrieNode(): isEnd(false), alpha(vector<TrieNode*>(26, nullptr)) {
-        
-    }
-};
-
-class Trie{
-
-    public:
-        TrieNode* node;
-        Trie(){
-            node = new TrieNode();
-        }
-    
-        void insert(string word) {
-            TrieNode* root = node;
-            int n = word.size();
-            for (int i = 0; i < n; i++) {
-                if (root->alpha[word[i] - 'a'] == nullptr) {
-                    root->alpha[word[i] - 'a'] = new TrieNode();
-                }
-                root = root->alpha[word[i] - 'a'];
-            }
-            root->isEnd = true;
-        }
-        
-        
-    
-};
-
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
         Trie* trie = new Trie();
-        for (string str: wordDict) {
+        for (auto str : wordDict) {
             trie->insert(str);
         }
         int len = s.size();
-        vector<int> dp(len + 1, false);
+        vector<int> dp(len + 1, 0);
         dp[0] = true;
         for (int i = 0; i <= len; i++) {
-
             if (dp[i]) {
-                TrieNode* ptr = trie->node;
+                TrieNode* ptr = trie->root;
                 for (int j = i; j < len; j++) {
                     if (ptr->alpha[s[j] - 'a'] == nullptr) {
                         break;
@@ -55,13 +21,55 @@ public:
                     ptr = ptr->alpha[s[j] - 'a'];
                     if (ptr->isEnd) {
                         dp[j + 1] = true;
-                        
-                    } 
+                        //i = j;
+                    }
                 }
             }
         }
-
-        
-        return dp[len];
+        return dp.back();
     }
+    
+private:
+    class TrieNode {
+        public:
+            vector<TrieNode*> alpha;
+            bool isEnd;
+            TrieNode(): isEnd(false), alpha(vector<TrieNode*>(26, nullptr)) 
+            {
+                
+            }
+            
+    };
+      
+    class Trie{
+        public:
+            TrieNode* root;
+            Trie() {
+                root = new TrieNode();
+            }
+            void insert(string word) {
+                TrieNode* ptr = root;
+                int len = word.size();
+                for (int i = 0; i < len; i++) {
+                    if (ptr->alpha[word[i] - 'a'] == nullptr) {
+                        ptr->alpha[word[i] - 'a'] = new TrieNode();
+                    }
+                    ptr = ptr->alpha[word[i] - 'a'];
+                }
+                ptr->isEnd = true;
+            }
+        
+           void free_all(TrieNode* curs){
+               if (!curs) {
+                  return;
+               }
+               for (int i = 0; i < 27; i++) {
+                   free_all(curs->alpha[i]);
+               }
+               delete curs;
+           }
+            ~Trie(){
+                free_all(root);
+            }
+    };
 };
