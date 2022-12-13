eaadbbc4a3779ac5ed50a76d3c8113e6c6aaa923
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-12-13 16:30:56 +0800

    Reduce code

--- 322. Coin Change.cpp
+++ 322. Coin Change.cpp
@@ -1,24 +1,20 @@
 // TC: O(n * amount)
 // SC: O(amount)
 class Solution {
-    
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int leastNumber[amount+1];
-        for(auto& i : leastNumber) 
-            i = INT_MAX;
+        vector<int> leastNumber(amount + 1, INT_MAX);
         leastNumber[0] = 0;
-                
-        for(int i = 1; i <= amount; i++){
-            int minNumber = INT_MAX;
-            for(auto& j : coins){
+        
+        for (int i = 1; i <= amount; i++) {
+            for (auto& j : coins) {
                 int sub = i - j;
-                if(sub >= 0 && leastNumber[sub] != INT_MAX){
-                    minNumber = min(minNumber, leastNumber[sub] + 1);
+                if (sub >= 0 && leastNumber[sub] != INT_MAX) {
+                    leastNumber[i] = min(leastNumber[i], leastNumber[sub] + 1);
                 }
-            }
-            leastNumber[i] = minNumber;
+            } 
         }
-        return leastNumber[amount] == INT_MAX ? -1 : leastNumber[amount];
+        
+        return leastNumber[amount] != INT_MAX ? leastNumber[amount] : -1;
     }
 };
