b6f4a0fa11457c613e80a0a58f48fe3e84751ed2
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-12-22 22:56:00 +0800

    Rewrite the code

--- 746. Min Cost Climbing Stairs.cc
+++ 746. Min Cost Climbing Stairs.cc
@@ -3,13 +3,13 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1, 0);
-        dp[0] = 0;
-        dp[1] = cost[0];
-        for (int i = 1; i < n; i++) {
-            dp[i + 1] = cost[i] + min(dp[i], dp[i - 1]);
+        int len = cost.size();
+        vector<int> dp (len, 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < len; i++) {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
         }
-        return min(dp[n], dp[n - 1]);
+        return min(dp[len - 1], dp[len - 2]);
     }
 };
