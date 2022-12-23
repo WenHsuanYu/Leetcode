b1d3c53b015cbea1dda39dc2d4e490f4c2acbba9
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-12-23 17:46:17 +0800

    Reduce code

--- CandidateSolution/300. Longest Increasing Subsequence_Reduced.cc
+++ CandidateSolution/300. Longest Increasing Subsequence_Reduced.cc
@@ -1,34 +1,33 @@
 // LIS reduced to LCS and then solve LCS question
 // O(nm)
-// O(nm)
+// O(m)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> sorted_nums = nums;
         sort(sorted_nums.begin(), sorted_nums.end());
-        vector<int>::iterator it = unique(sorted_nums.begin(), sorted_nums.end());
+        auto it = unique(sorted_nums.begin(), sorted_nums.end());
         sorted_nums.erase(it, sorted_nums.end());
-        
-        int ans = [&](){
+
+        auto ans = [&]() {
             int n = nums.size();
             int m = sorted_nums.size();
-
-            vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-            
+            vector<int> dp(m + 1, 0);
             for (int i = 1; i <= n; i++) {
+                int prev = dp[0];
                 for (int j = 1; j <= m; j++) {
+                    int tmp = dp[j];
                     if (nums[i - 1] == sorted_nums[j - 1]) {
-                        dp[i][j] = 1 + dp[i - 1][j - 1];
-
+                        dp[j] = prev + 1;
                     } else {
-                        dp[i][j] = dp[i - 1][j] > dp[i][j - 1] ? dp[i - 1][j] : dp[i][j - 1];
-                    }     
+                        dp[j] = max(dp[j], dp[j - 1]);
+                    }
+                    prev = tmp;
                 }
+                cout << endl;
             }
-            
-            return dp[n][m];
+            return dp[m];
         }();
         return ans;
-        
     }
 };
