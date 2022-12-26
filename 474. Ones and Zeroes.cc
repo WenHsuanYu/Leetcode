c108897dd65f6d4d99a73ac53c780f4c9314e843
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-12-27 01:11:37 +0800

    Add comment

--- 474. Ones and Zeroes.cc
+++ 474. Ones and Zeroes.cc
@@ -8,6 +8,9 @@ public:
         for (const string& s : strs) {
             int zeros = count(s.begin(), s.end(), '0');
             int ones = s.size() - zeros;
+            //It can not run from 0 to m because it will compute the same string twice and 
+            //then the result is wrong and larger than the size of string.
+            //In summary, this question can take every element only once, not repeatedly, if you want
             for (int i = m; i >= zeros; i--) {
                 for (int j = n; j >= ones; j--) {
                     dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1);
