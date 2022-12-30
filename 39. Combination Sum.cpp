fa86531d8cfbce0a450d33f9f702446d087ca2cf
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-12-30 23:59:41 +0800

    Reduce code

--- 39. Combination Sum.cpp
+++ 39. Combination Sum.cpp
@@ -2,33 +2,27 @@
 // SC: O(m)
 class Solution {
 public:
-    vector<vector<int>> result;
-    vector<int> temp;
-    
-    void recur(vector<int>& candidates, int target, int i) {
-            if (target < 0) {
-                return;
-            }
-            
-            if (target == 0) {
-                result.push_back(temp);
-                return;
-            }
-            
-            while (i < candidates.size() && target - candidates[i] >= 0) {
-                temp.push_back(candidates[i]);
-                recur(candidates, target - candidates[i], i);
-                ++i;
-                temp.pop_back();
-                
-            }
-        
-    }
-    
+    vector<vector<int>> res;
+    vector<int> tmp;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        recur(candidates, target, 0);
-        return result;
-    
+        dfs(0, candidates, target);
+        return res;
     }
+
+private:
+    void dfs(int idx, vector<int>& candidates, int target) {
+        if (target <= 0) {
+            if (!target) {
+                res.push_back(tmp);
+            }
+            return;
+        }
+        for (int i = idx; i < candidates.size(); i++) {
+            tmp.push_back(candidates[i]);
+            dfs(i, candidates, target - candidates[i]);
+            tmp.pop_back();
+        }
+    }
+
 };
