55e52917d853b426e147ee491baca254261c13b3
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2022-12-31 01:55:44 +0800

    Reduce code

--- CandidateSolution/46. Permutations_DFS_variant.cc
+++ CandidateSolution/46. Permutations_DFS_variant.cc
@@ -2,35 +2,28 @@
 // SC: O(n)
 class Solution {
 public:
-    vector<int> temp, visited;
+    vector<int> tmp;
+    vector<int> visited;
     vector<vector<int>> res;
-    
-    void dfs(vector<int>& nums, int level) {
-        
-        if (level == nums.size()) {
-            res.push_back(temp);
-            return;
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (visited[i] == true)
-                continue;   
-            
-            visited[i] = 1;
-            temp.push_back(nums[i]);
-            dfs(nums, level + 1);
-            temp.pop_back();
-            visited[i] = 0; 
-        }
-        
-        
-    }
-    
     vector<vector<int>> permute(vector<int>& nums) {
         visited.resize(nums.size(), 0);
-        dfs(nums, 0);
-        
-        
+        dfs(nums);
         return res;
+    }
+    void dfs(vector<int>& nums) {
+        if (tmp.size() == nums.size()) {
+            res.push_back(tmp);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = 1;
+            tmp.push_back(nums[i]);
+            dfs(nums);
+            visited[i] = 0;
+            tmp.pop_back();
+        }
     }
 };
