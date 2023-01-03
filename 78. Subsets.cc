e24416c5724cc0710ffb8d05a062fbf224f83d9a
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2023-01-03 15:05:24 +0800

    Add another implementation

--- 78. Subsets.cc
+++ 78. Subsets.cc
@@ -24,3 +24,22 @@ public:
         return ans;
     }
 };
+
+// or another implementation
+class Solution {
+public:
+    vector<vector<int>> res_vec = {{}};
+    vector<int> res;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        dfs(0, nums);
+        return res_vec;
+    }
+    void dfs(int idx, vector<int> &nums) {
+        for (int i = idx; i < nums.size(); ++i) {
+            res.push_back(nums[i]);
+            res_vec.push_back(res);
+            dfs(i+1, nums);
+            res.pop_back();
+        }
+    }
+};
