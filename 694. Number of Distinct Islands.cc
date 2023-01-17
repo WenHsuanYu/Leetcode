26d46df2e53c204513221db7bae2e786fabd3fb5
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2023-01-17 22:25:38 +0800

    Reduce the code

--- 694. Number of Distinct Islands.cc
+++ 694. Number of Distinct Islands.cc
@@ -1,52 +1,46 @@
 // TC: O(mn)
 // SC: O(mn)
-#include <iostream>
 #include <bits/stdc++.h>
-
+#include <unordered_set>
 using namespace std;
+
 
 class Solution {
 public:
-    vector<vector<int>> dirs{{0,-1},{-1,0},{0,1},{1,0}};
+    int row;
+    int col;
+    vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    unordered_set<string> set;
     int numDistinctIslands(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        unordered_set<string>res;
-        vector<vector<int>>visited(m, vector<int>(n, 0));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1 && visited[i][j] == 0) {
-                    unordered_set<string> s;
-                    helper(grid, i, j, i, j, visited, s);
-                    string tmp = "";
-                    for (auto str : s) {
-                        tmp += str + "-";
-                    }
-                    res.insert(tmp);
+        row = grid.size();
+        col = grid[0].size();
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (grid[i][j]) {
+                    string code = "";
+                    dfs(i, j, grid, code);
+                    set.insert(code);
                 }
             }
         }
-        return res.size();
-
+        return set.size();
     }
-    void helper(vector<vector<int>>& grid, int x0, int y0, int i, int j, vector<vector<int>>& visited, unordered_set<string>& s) {
-        int m = grid.size();
-        int n = grid[0].size();
-        visited[i][j] = 1;
-        for (auto dir : dirs) {
-            int x = i + dir[0];
-            int y = j + dir[1];
-            if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || grid[x][y] == 0) {
+    void dfs(int r, int c, vector<vector<int>> &grid, string &code) {
+        grid[r][c] = 0;
+        for (int i = 0; i < (int)dirs.size(); ++i) {
+            int nr = r + dirs[i][0];
+            int nc = c + dirs[i][1];
+            if (nr < 0 || nr >= row || nc < 0 || nc >= col || !grid[nr][nc]) {
                 continue;
             }
-            string str = to_string(x - x0) + "_" + to_string(y - y0);
-            s.insert(str);
-            helper(grid, x0, y0, x, y, visited, s);
-        }        
+            code += to_string(i) + ",";
+            dfs(nr, nc, grid, code);
+        }
     }
 };
 
-int main(int argc, char* argv[]) {
+
+int main(int argc, char** argv){
     Solution solution;
     vector<vector<int>> grid{{1,1,1,0,0,0}, {1,1,1,0,0,0}, {0,0,0,1,1,0}, {0,0,0,1,1,1}};
     int res = solution.numDistinctIslands(grid);
